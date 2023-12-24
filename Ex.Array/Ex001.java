6a79b0429d4495525dd0f41d6232c07fda66fafa
Gabriel Henrique <gabriel.h.miranda.rodrigues@gmail.com>
2023-12-24 13:24:50 -0300

    SET locale

--- Ex.Array/Ex001.java
+++ Ex.Array/Ex001.java
@@ -5,10 +5,12 @@
 these people.
  */
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Ex001 {
     public static void main(String[] args) {
+         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
         System.out.println("How many heights are you going to type? ");
