ce748a6401fc1a3fdf6a9e36a7ff5a95349de0b9
Gabriel Henrique <gabriel.h.miranda.rodrigues@gmail.com>
2023-12-24 13:21:57 -0300

    Ex001 vector

---
+++ Ex.Array/Ex001.java
@@ -0,0 +1,35 @@
+/*
+ *
+ * Create a program to read an integer N and the height of N people.
+  Store the N heights in an array. Then, display the average height of
+these people.
+ */
+
+import java.util.Scanner;
+
+public class Ex001 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("How many heights are you going to type? ");
+        int number = sc.nextInt();
+        double[] vect = new double[number];
+
+        for (int i = 0; i < number; i++) {
+            System.out.println("Type a height: ");
+            vect[i] = sc.nextDouble();
+        }
+
+        double height = 0;
+
+        for (int i = 0; i < vect.length; i++) {
+
+            height = height + vect[i];
+
+        }
+
+        System.out.println("Average height: " + height / vect.length);
+
+        sc.close();
+    }
+}
