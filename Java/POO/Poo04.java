f5758efabf1d4e472fe89f662d89ead16f00f500
Gabriel Henrique <gabriel.h.miranda.rodrigues@gmail.com>
2023-12-28 00:46:49 -0300

    Update poo04

--- Java/POO/Poo04.java
+++ Java/POO/Poo04.java
@@ -2,7 +2,8 @@ package Exercicios.Java.POO;
 
 public class Poo04 {
     public static void main(String[] args) {
-        System.out.println("Hello GitHub!");
+        System.out.println("Hello GitHub!!!");
+        
     }
     
 }
