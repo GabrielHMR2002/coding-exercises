0d9b5594eefc730a24410c069b79eff4e4f17a88
gabriel <gabriel.h.miranda.rodrigues@gmail.com>
2023-12-30 16:56:15 -0300

    js009 function

---
+++ JavaScript/script009.js
@@ -0,0 +1,16 @@
+
+function myName() {
+    console.log("Gabriel Henrique")
+}
+myName();
+
+
+
+function mulTwo(value) {
+    return value * 2
+}
+console.log(mulTwo(4))
+
+let x = mulTwo(2)
+
+console.log(x)
