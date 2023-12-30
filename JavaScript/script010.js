9500fd431ff3e9705256540c4eb1d15101afb59b
gabriel <gabriel.h.miranda.rodrigues@gmail.com>
2023-12-30 17:04:25 -0300

    js010 Math Operators

---
+++ JavaScript/script010.js
@@ -0,0 +1,18 @@
+console.log('Math Operators')
+
+// Arithmetic Operators
+
+let balance = 10
+
+console.log(balance + balance)
+console.log(balance * balance)
+console.log(balance ** balance)
+
+// ++ and -- Increment Operators
+
+let age = 21;
+
+age++
+
+console.log(age++)
+console.log(age)
