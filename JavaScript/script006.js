2623796be007b1239ae9c6cf5a2016223feaef4d
gabriel <gabriel.h.miranda.rodrigues@gmail.com>
2023-12-30 15:56:48 -0300

    js006 object

---
+++ JavaScript/script006.js
@@ -0,0 +1,11 @@
+
+//typeof person == object
+let person = {
+    //its a object
+    //key    value
+    name : 'Gabriel', //typeof person.name == string
+    age : 21, //typeof person.age == number
+    male : true //typeof person.male == boolean
+}
+
+console.log(person.name)
