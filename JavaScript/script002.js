7173a17d60c62df1cd9b3e51dc4e426f1e7291be
gabriel <gabriel.h.miranda.rodrigues@gmail.com>
2023-12-30 15:18:55 -0300

    js002

---
+++ JavaScript/script002.js
@@ -0,0 +1,13 @@
+let myName = 'Gabriel';
+let myAge = 21;
+
+console.log("My name is ", myName ," and my age is ", myAge)
+
+// let name != let NAME
+
+let name, age; // undefined
+// You can declarate multi variables in the same line, but it is better if you declarate one per line.
+
+
+console.log(name, age)
+
