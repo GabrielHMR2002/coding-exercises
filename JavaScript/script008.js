a1b6ba1019c515deeb99e8d9cdc49472850c9a8e
gabriel <gabriel.h.miranda.rodrigues@gmail.com>
2023-12-30 16:09:40 -0300

    js008 function

---
+++ JavaScript/script008.js
@@ -0,0 +1,21 @@
+//function
+
+
+function sum(a, b) {
+    sumNumbers = a + b
+    return sumNumbers
+}
+
+console.log(sum(10, 5))
+
+
+let a = 10
+
+function changeValue(value) {
+
+    a = value
+}
+
+changeValue(5)
+
+console.log(a)
