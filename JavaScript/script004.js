b2ef592afae1e4576f6dd0900e8fc50c0ff354a8
gabriel <gabriel.h.miranda.rodrigues@gmail.com>
2023-12-30 15:30:38 -0300

    js004

---
+++ JavaScript/script004.js
@@ -0,0 +1,20 @@
+let name = 'Gabriel Henrique' //string literal
+console.log('name: ' + name)
+
+let age = 21
+console.log('Age: 0', age)
+
+let bool = true
+console.log(bool)
+console.log(!bool)
+
+
+let house = undefined // its not common to define a variable as undefined 
+console.log(house)
+
+let color = null //to reset a value, to redefine a value
+console.log(color)
+
+
+
+
