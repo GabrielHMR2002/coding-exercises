68a453ec05a659c51b17228a16e3712bc91a80fa
gabriel <gabriel.h.miranda.rodrigues@gmail.com>
2023-12-30 15:38:03 -0300

    js005

---
+++ JavaScript/script005.js
@@ -0,0 +1,20 @@
+// using js you dont need to declaret the type of a variable
+
+// just declarte the variable
+
+//in run time the js can know the type
+
+// use typeof in console to see
+
+console.log('Using typeof you can see the type of any variable: ')
+
+let n1 = 20 //typeof number
+
+let n2 = 20.75 //typeof number
+
+let age = true // typeof boolean
+
+let name //typeof undefined
+
+let country = null // typeof object
+
