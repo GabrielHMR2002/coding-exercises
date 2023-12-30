b923dffee1feaf409271ef0153ca5b3853651084
gabriel <gabriel.h.miranda.rodrigues@gmail.com>
2023-12-30 16:02:39 -0300

    js 007 array

---
+++ JavaScript/script007.js
@@ -0,0 +1,14 @@
+// Array
+
+
+let family = ['gabriel', 'henrique', 'miranda', 'rodrigues', 21, true]
+console.log(family)
+console.log(family[0])
+console.log(family[1])
+console.log(family[2])
+console.log(family[3])
+console.log(family[4])
+console.log(family[5])
+console.log(!family[5])
+
+console.log(family.length)
